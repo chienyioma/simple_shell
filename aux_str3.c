cdcebe6bc747e99eb2173edcfdbdede5b52c00ad
chienyioma <chienyioma19@gmail.com>
2022-09-07 12:36:32 -0700

    Simple Shell Project

---
+++ aux_str3.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+
+/**
+ * rev_string - reverses a string.
+ * @s: input string.
+ * Return: no return.
+ */
+void rev_string(char *s)
+{
+	int count = 0, i, j;
+	char *str, temp;
+
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+			break;
---
+++ cd_shell.c
@@ -0,0 +1,26 @@
+#include "shell.h"
+
+/**
+ * cd_shell - changes current directory
+ *
+ * @datash: data relevant
+ * Return: 1 on success
+ */
+int cd_shell(data_shell *datash)
+{
+	char *dir;
+	int ishome, ishome2, isddash;
+
+	dir = datash->args[1];
+
+	if (dir != NULL)
+	{
+		ishome = _strcmp("$HOME", dir);
+		ishome2 = _strcmp("~", dir);
+		isddash = _strcmp("--", dir);
+	}
+
+	if (dir == NULL || !ishome || !ishome2 || !isddash)
+	{
+		cd_to_home(datash);
+		return (1);
